e898b73e7077cebe23627507e432c4e5f3642179
Suzana Pachelle <suzi.pachelle@gmail.com>
2024-11-26 10:50:28 -0300

    Screen code integrated with the bluetooth library - not tested

--- app/screens/SyncScreen.js
+++ app/screens/SyncScreen.js
@@ -11,7 +11,7 @@ import {
   ActivityIndicator,
 } from "react-native";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import useBLE from "../services/useBLE";
 import DeviceModal from "../components/deviceConnectionModal";
 
@@ -25,36 +25,36 @@ function SyncScreen({ navigation }) {
     disconnectFromDevice,
     dataArray,
     clearDataArray,
-    isReceivingFinished
+    isReceivingFinished,
   } = useBLE();
-  
+
   const [isModalVisible, setIsModalVisible] = useState(false);
-  
+
   const scanForDevices = async () => {
     const isPermissionsEnabled = await requestPermissions();
     if (isPermissionsEnabled) {
-        console.log("Scanning for Devices");
-        scanForPeripherals();
+      console.log("Scanning for Devices");
+      scanForPeripherals();
     }
   };
-  
+
   const openModal = async () => {
-    console.log("Opening Modal & Scanning for Devices")
+    console.log("Opening Modal & Scanning for Devices");
     scanForDevices();
     setIsModalVisible(true);
   };
 
   const hideModal = () => {
-      setIsModalVisible(false);
+    setIsModalVisible(false);
   };
 
   const storeNewProcesses = async (newProcessesArray) => {
     console.log("Storing New Processes");
-    try{
+    try {
       const fetchedProcesses = await AsyncStorage.getItem("processes");
       if (!fetchedProcesses) {
-          await AsyncStorage.setItem("processes", JSON.stringify([]));
-          console.log("Processes Initialized");
+        await AsyncStorage.setItem("processes", JSON.stringify([]));
+        console.log("Processes Initialized");
       }
       const parsedProcesses = JSON.parse(fetchedProcesses);
       // push every new activity to the parsedActivities array
@@ -65,28 +65,29 @@ function SyncScreen({ navigation }) {
         parsedProcesses.push(JSON.parse(newProcess));
         console.log(newProcess);
       }
-      await AsyncStorage.setItem("processes", JSON.stringify(parsedProcesses)).then(() => {
-          console.log("Processes Stored");
+      await AsyncStorage.setItem(
+        "processes",
+        JSON.stringify(parsedProcesses)
+      ).then(() => {
+        console.log("Processes Stored");
       });
     } catch (error) {
       console.log(error);
     }
   };
-  
+
   if (isReceivingFinished === true) {
     console.log(dataArray);
     storeNewProcesses(dataArray);
     clearDataArray();
     console.log("Disconnecting from Device");
     disconnectFromDevice();
-  }
-  else {
+  } else {
     console.log("Syncing in Progress...");
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      
       <Image
         source={require("../assets/prime_corn_logo.png")}
         style={styles.image}
@@ -94,33 +95,30 @@ function SyncScreen({ navigation }) {
 
       <View style={styles}>
         {connectedDevice ? (
-        <>
+          <>
             <ActivityIndicator size="large" color="Pink" />
             <Text style={styles}>Syncing...</Text>
-        </>
+          </>
         ) : (
-        <Text style={styles}>
-            Sync to your last activity
-        </Text>
+          <Text style={styles}>Sync to your last activity</Text>
         )}
       </View>
 
       <TouchableOpacity
-          onPress={connectedDevice ? disconnectFromDevice : openModal}
-          style={styles.syncButton}
+        onPress={connectedDevice ? disconnectFromDevice : openModal}
+        style={styles.syncButton}
       >
-          <Text style={styles}>
+        <Text style={styles}>
           {connectedDevice ? "Disconnect" : "Search bike module"}
-          </Text>
+        </Text>
       </TouchableOpacity>
-      
+
       <DeviceModal
-          closeModal={hideModal}
-          visible={isModalVisible}
-          connectToPeripheral={connectToDeviceOnReceiving}
-          device={device}
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDeviceOnReceiving}
+        device={device}
       />
-      
     </SafeAreaView>
   );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, Image, View, Button, FlatList, Text, TouchableOpacity } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  View,
+  Button,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function WelcomeScreen({ navigation }) {
-
   const [refreshing, setRefreshing] = useState(false);
   const [processes, setProcesses] = useState([]);
-  
+
   console.log(processes);
 
   const fetchData = async () => {
@@ -16,27 +24,37 @@ function WelcomeScreen({ navigation }) {
       const fetchedProcesses = await AsyncStorage.getItem("processes");
       if (fetchedProcesses) {
         const parsedProcesses = JSON.parse(fetchedProcesses);
-  
-        const array = parsedProcesses.map((item) => {
-          try {
-            const parsedItem = typeof item === "string" ? JSON.parse(item) : item;
-  
-            // Extract the classification summary into an array
-            const { Pure, Broken, Discolored, Silkcut, Unknown } = parsedItem.classificationSummary;
-            const arrayClassificationSummary = [Pure, Broken, Discolored, Silkcut, Unknown];
-  
-            // Return the formatted object
-            return {
-              processId: parsedItem.processId,
-              classificationSummary: arrayClassificationSummary,
-              timestamp: parsedItem.timestamp,
-            };
-          } catch (error) {
-            console.error("Error parsing item:", error, item);
-            return null; // Return null for problematic items
-          }
-        }).filter((item) => item !== null); // Filter out null items
-  
+
+        const array = parsedProcesses
+          .map((item) => {
+            try {
+              const parsedItem =
+                typeof item === "string" ? JSON.parse(item) : item;
+
+              // Extract the classification summary into an array
+              const { Pure, Broken, Discolored, Silkcut, Unknown } =
+                parsedItem.classificationSummary;
+              const arrayClassificationSummary = [
+                Pure,
+                Broken,
+                Discolored,
+                Silkcut,
+                Unknown,
+              ];
+
+              // Return the formatted object
+              return {
+                processId: parsedItem.processId,
+                classificationSummary: arrayClassificationSummary,
+                timestamp: parsedItem.timestamp,
+              };
+            } catch (error) {
+              console.error("Error parsing item:", error, item);
+              return null; // Return null for problematic items
+            }
+          })
+          .filter((item) => item !== null); // Filter out null items
+
         // Update state with the processed array
         setProcesses(array);
       } else {
@@ -48,28 +66,74 @@ function WelcomeScreen({ navigation }) {
       console.error("Error fetching data:", error);
     }
   };
-  
 
   const handleRefresh = () => {
     setRefreshing(true);
     fetchData().then((processes) => {
-        if (processes) {
-            console.log("Processes fetched");
-            setRefreshing(false);
-        }
-        else {
-            console.log("Failed to fetch processes");
-            setRefreshing(false);
-        }
+      if (processes) {
+        console.log("Processes fetched");
+        setRefreshing(false);
+      } else {
+        console.log("Failed to fetch processes");
+        setRefreshing(false);
+      }
     });
-  }
+  };
 
   useEffect(() => {
     const loadProcesses = async () => {
       await fetchData();
     };
     loadProcesses(); // Call the async function
-  }, []); // Empty dependency array to run only once  
+  }, []); // Empty dependency array to run only once
+
+  const RenderItem = ({ item }) => {
+    const totalSeeds =
+      item.classificationSummary[0] +
+      item.classificationSummary[1] +
+      item.classificationSummary[2] +
+      item.classificationSummary[3] +
+      item.classificationSummary[4];
+    const irregularSeeds =
+      item.classificationSummary[1] +
+      item.classificationSummary[2] +
+      item.classificationSummary[3] +
+      item.classificationSummary[4];
+
+    return (
+      <View style={styles.card}>
+        {/* Data */}
+        <Text style={styles.date}>{item.timestamp}</Text>
+        <View style={styles.divider} />
+
+        {/* Total de sementes */}
+        <Text style={styles.totalSeeds}>Total Seeds: {totalSeeds}</Text>
+        <View style={styles.divider} />
+
+        {/* Regular e irregular seeds */}
+        <View style={styles.regularIrregularContainer}>
+          <View style={styles.border}>
+            <Text style={styles.regular}>
+              Regular seeds:{" "}
+              <Text style={styles.light}>{item.classificationSummary[0]}%</Text>
+            </Text>
+          </View>
+          <Text style={styles.irregular}>
+            Irregular seeds: <Text style={styles.light}>{irregularSeeds}%</Text>
+          </Text>
+        </View>
+        <View style={styles.divider} />
+
+        {/* Botão de relatório */}
+        <TouchableOpacity
+          style={styles.reportButton}
+          onPress={() => navigation.navigate("Report", { item })}
+        >
+          <Text style={styles.reportButtonText}>View Report</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <>
@@ -77,11 +141,25 @@ function WelcomeScreen({ navigation }) {
         source={require("../assets/prime_corn_logo.png")}
         style={styles.image}
       />
-
-    {/* TODO: FLATLIST ITERATING OVER THE PROCESSES */}
-    {/* THIS IS JUST FOR DEBUG */}
-    {/* CREATE A COMPONENT FOR THE INFOS AND ONDE IT IS CLICKED IT IS REDIRECTED TO THE PROCESS PAGE WITH THE DATA AS PARAM */}
       <FlatList
+        data={processes}
+        renderItem={RenderItem}
+        keyExtractor={item.processId}
+        contentContainerStyle={styles.listContainer}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
+      />
+      <TouchableOpacity
+        style={styles.syncButton}
+        onPress={() => navigation.navigate("Sync")}
+      >
+        <Text style={styles.reportButtonText}>Sync</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+      {/* TODO: FLATLIST ITERATING OVER THE PROCESSES */}
+      {/* THIS IS JUST FOR DEBUG */}
+      {/* CREATE A COMPONENT FOR THE INFOS AND ONDE IT IS CLICKED IT IS REDIRECTED TO THE PROCESS PAGE WITH THE DATA AS PARAM */}
+      {/* <FlatList
         data={processes}
         renderItem={({ item }) => (
           <View>
@@ -97,19 +175,7 @@ function WelcomeScreen({ navigation }) {
         )}
         refreshing={refreshing}
         onRefresh={handleRefresh}
-      />
-
-      <Button
-        style={styles}
-        title="Syncsds"
-        onPress={() => navigation.navigate("Sync")}
-      ></Button>
-
-      <Button
-        style={styles.reportButton}
-        title="Report"
-        onPress={() => navigation.navigate("Report")}
-      ></Button>
+      /> */}
     </>
   );
 }
@@ -119,21 +185,99 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
+    backgroundColor: "#f9f9f9",
   },
   image: {
     width: 100,
     height: 100,
     marginTop: 20,
   },
-  syncButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
+  listContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  },
+  card: {
+    width: 320,
+    height: 210,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    marginHorizontal: 8, // Container mais largo
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  date: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#333",
+    marginBottom: 8,
+    flex: 1,
+    textAlign: "center",
+  },
+  totalSeeds: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#333",
+    marginVertical: 8,
+    flex: 1,
+    textAlign: "center",
+  },
+  regularIrregularContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 8,
+  },
+  border: {
+    borderRightWidth: 1,
+    borderRightColor: "#e5e5e5",
+    paddingRight: 8,
+    flex: 1,
+  },
+  regular: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#333",
+  },
+  irregular: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#333",
+    flex: 1,
+    textAlign: "center",
+  },
+  light: {
+    fontSize: 14,
+    color: "#666",
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e5e5",
+    marginVertical: 8,
   },
   reportButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
+    backgroundColor: "#E0BBE4",
+    borderRadius: 8,
+    paddingVertical: 10,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  syncButton: {
+    backgroundColor: "#E0BBE4",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  reportButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
 
