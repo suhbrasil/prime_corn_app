5a3866ec1f2789793565217c5a24aa9d04ade470
Eduardo Ximenes <duduximenes470@gmail.com>
2024-12-20 18:41:06 -0300

    fix: miner fixes

--- app/screens/ReportScreen.js
+++ app/screens/ReportScreen.js
@@ -47,7 +47,7 @@ function ReportScreen({ route }) {
     const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
     const day = String(date.getDate()).padStart(2, "0");
     const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
+    return `${day}/${month}/${year}`;
   };
 
   return (
